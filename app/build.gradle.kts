a3fafa84730eb40a01aaa7ac658ed6fbdf034502
Daria Chugunova <dariachugunova2003@gmail.com>
2024-03-08 18:20:31 +0300

    add res

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.daria.example.quizapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.daria.example.quizapp"
