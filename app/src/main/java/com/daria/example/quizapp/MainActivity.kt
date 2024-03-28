3b6f02aeba616908e605e9f7fe641b14f6e2e75f
robarcoo <111498279+robarcoo@users.noreply.github.com>
2024-03-28 11:49:23 +0300

    added modules

--- app/src/main/java/com/daria/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/daria/example/quizapp/MainActivity.kt
@@ -3,10 +3,26 @@ package com.daria.example.quizapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.daria.example.quizapp.ui.theme.QuizAppTheme
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val viewModel: QuizViewModel by viewModels()
+        setContent {
+            QuizAppTheme {
+                Surface(modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background) {
+
+                }
+            }
+
+        }
     }
 }
